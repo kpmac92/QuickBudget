e3c176199fc38527ee82664c3bb5279ecfebcf22
Kevin Patrick <kpmac92@gmail.com>
2017-02-20 15:54:16 -0500

    Add proper landing page and navigation links

--- quickBudget/urls.py
+++ quickBudget/urls.py
@@ -7,5 +7,6 @@ app_name = 'quickBudget'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     # ex: /budget/5/
-    url(r'^(?P<budget_id>[0-9]+)/$', views.budget, name='budget'),
+    #url(r'^(?P<budget_id>[0-9]+)/$', views.budget, name='budget'),
+    url(r'^budget$', views.budget, name='budget'),
 ]
--- quickBudget/views.py
+++ quickBudget/views.py
@@ -6,13 +6,13 @@ from .models import BudgetCategory
 
 # Create your views here.
 def index(request):
-    return HttpResponse("ayyyy lmao")
+    return render(request, 'quickBudget/index.html')
 
 
-def budget(request, budget_id):
+def budget(request):
     categories_list = BudgetCategory.objects.order_by('-id')
     context = {
         'categories_list': categories_list,
     }
-    return render(request, 'quickBudget/index.html', context)
+    return render(request, 'quickBudget/budget.html', context)
 
